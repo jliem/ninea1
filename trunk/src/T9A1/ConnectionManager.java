53c1e89365db0c33b30e4bb688a8093dd3593f5b
chaseb27 <chaseb27@e86a6e46-f6f9-11dd-8c4e-4bbb75a408b7>
2009-02-25 16:19:20 +0000

    This is a more updated stub for connection manager. ~CB

--- trunk/src/T9A1/ConnectionManager.java
+++ trunk/src/T9A1/ConnectionManager.java
@@ -1,24 +1,44 @@
 package T9A1;
-
 /**
  * Class
  * @author Chase
  */
 
 public class ConnectionManager {
-
+	/**
+	 * Variables
+	 * kioskID: identifies the kiosk
+	 * kioskPassword: allows the kiosk to access the server
+	 */
+	private char[] kioskID;
+	private char[] kioskPassword;
 	public ConnectionManager(){
 
 	
 	}
 	
-	public void RequestConnection(){
+	public String[] serializeRequest(){
 		
-		
+		return null;
 	}
+
 	
-	public void Connect(){
-		
+	
+	
+	public void setKioskPassword(char[] kioskPassword) {
+		this.kioskPassword = kioskPassword;
+	}
+
+	public char[] getKioskPassword() {
+		return kioskPassword;
+	}
+
+	public void setKioskID(char[] kioskID) {
+		this.kioskID = kioskID;
+	}
+
+	public char[] getKioskID() {
+		return kioskID;
 	}
 	
 	
