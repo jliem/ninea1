e729ed1c643407927c83433428a76637a8e52043
chaseb27 <chaseb27@e86a6e46-f6f9-11dd-8c4e-4bbb75a408b7>
2009-02-25 02:32:51 +0000

    Client side ConnectionManager class added. ~Chase

---
+++ trunk/src/T9A1/ConnectionManager.java
@@ -0,0 +1,27 @@
+package T9A1;
+
+/**
+ * Class
+ * @author Chase
+ */
+
+public class ConnectionManager {
+
+	public ConnectionManager(){
+
+	
+	}
+	
+	public void RequestConnection(){
+		
+		
+	}
+	
+	public void Connect(){
+		
+	}
+	
+	
+	
+	
+}
