c39b59b1947c49ce6c4441d892fa96183634d308
johannesliem <johannesliem@e86a6e46-f6f9-11dd-8c4e-4bbb75a408b7>
2009-03-24 21:22:41 +0000

    More CacheManager tests

--- trunk/test/T9A1/client/data/CacheManagerTest.java
+++ trunk/test/T9A1/client/data/CacheManagerTest.java
@@ -12,7 +12,12 @@ import junit.framework.TestCase;
 public class CacheManagerTest extends TestCase {
 
 	private CacheManager cm;
+	private HashMap<String, CacheEntry> map;
+	private PriorityQueue<CacheEntry> heap;
+
 	private ArrayList<Item> itemList;
+	private String[] queries;
+
 	private final String NOT_IN_CACHE = "query not in cache";
 	private final String IN_CACHE = "query in cache";
 
@@ -25,10 +30,17 @@ public class CacheManagerTest extends TestCase {
 
 		itemList.add(one);
 		itemList.add(two);
+
+		queries = new String[5];
+		for (int i=0; i<queries.length; i++) {
+			queries[i] = "Test Query " + (i+1);
+		}
 	}
 
 	public void setUp() {
 		cm = new CacheManager();
+		map = cm.getMap();
+		heap = cm.getHeap();
 	}
 
 	public void testDoSearch() {
@@ -75,10 +87,36 @@ public class CacheManagerTest extends TestCase {
 		assertFalse(isHit);
 	}
 
-	public void testRemoval() {
-		HashMap<String, CacheEntry> map = cm.getMap();
-		PriorityQueue<CacheEntry> heap = cm.getHeap();
+	public void testLruRemoval() {
+		for (String query : queries) {
+			cm.add(query, itemList);
+		}
 
+		cm.setMaxCacheEntries(5);
+
+		// Access query 1 one time, query 2 two times, etc. so query
+		// 5 will be accessed five times
+		for (int i=0; i<queries.length; i++) {
+			for (int j=0; j<(i+1); j++) {
+				cm.doSearch(queries[j]);
+			}
+		}
+
+		// Add another query. Query 1 should be removed from the cache.
+		String testQuery = "new test query";
+		cm.add(testQuery, null);
+
+		assertFalse(cm.contains(queries[0]));
+		assertTrue(cm.contains(testQuery));
+		assertTrue(cm.contains(queries[1]));
+
+		// Add another query. The test query we just added should be removed.
+		cm.add(queries[0], null);
+		assertFalse(cm.contains(testQuery));
+		assertTrue(cm.contains(queries[0]));
+	}
+
+	public void testDirectRemoval() {
 		cm.add(this.IN_CACHE, itemList);
 
 		// Construct a CacheEntry that looks like the one
@@ -92,9 +130,8 @@ public class CacheManagerTest extends TestCase {
 	}
 
 	public void testAdd() {
-		HashMap<String, CacheEntry> map = cm.getMap();
-		PriorityQueue<CacheEntry> heap = cm.getHeap();
 
+		// Test single add
 		cm.add(this.IN_CACHE, itemList);
 
 		// Construct a CacheEntry that looks like the one
@@ -106,12 +143,45 @@ public class CacheManagerTest extends TestCase {
 
 		assertEquals(map.size(), 1);
 		assertEquals(heap.size(), 1);
+
+		// Test multiple adds
+		cm.clear();
+
+		for (int i=0; i<queries.length; i++) {
+			cm.add(queries[i], itemList);
+			entry = new CacheEntry(queries[i], itemList);
+
+			assertTrue(map.containsKey(queries[i]));
+			assertTrue(heap.contains(entry));
+
+			assertEquals(map.size(), i+1);
+			assertEquals(heap.size(), i+1);
+		}
+	}
+
+	public void testClear() {
+		cm.add(this.IN_CACHE, itemList);
+
+		// Construct a CacheEntry that looks like the one
+		// used in thePriorityQueue
+		CacheEntry entry = new CacheEntry(this.IN_CACHE, itemList);
+
+		assertTrue(map.containsKey(this.IN_CACHE));
+		assertTrue(heap.contains(entry));
+
+		assertEquals(map.size(), 1);
+		assertEquals(heap.size(), 1);
+
+		cm.clear();
+
+		assertFalse(map.containsKey(this.IN_CACHE));
+		assertFalse(heap.contains(entry));
+
+		assertEquals(map.size(), 0);
+		assertEquals(heap.size(), 0);
 	}
 
 	public void testHeapCount() {
-		HashMap<String, CacheEntry> map = cm.getMap();
-		PriorityQueue<CacheEntry> heap = cm.getHeap();
-
 		String query = this.IN_CACHE;
 
 		cm.add(query, itemList);
