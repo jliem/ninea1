6a5fb2227f93aa6abf4cb7d5f21f973eb1572864
johannesliem <johannesliem@e86a6e46-f6f9-11dd-8c4e-4bbb75a408b7>
2009-03-26 18:29:00 +0000

    Adding some javadoc, removed unused class IKioskClientFactory

--- trunk/src/T9A1/client/data/ICacheManager.java
+++ trunk/src/T9A1/client/data/ICacheManager.java
@@ -6,13 +6,40 @@ import T9A1.common.Item;
 
 public interface ICacheManager {
 
+	/**
+	 * Adds an entry to the cache.
+	 *
+	 * @param query the search query
+	 * @param items the list of items to associate with this query
+	 */
 	public void add(String query, List<Item> items);
 
+	/**
+	 * Checks whether this query is both in the cache and is not stale.
+	 *
+	 * @param query the search query
+	 * @return true if the query is a cache hit (in the cache and not stale),
+	 * false otherwise.
+	 */
 	public boolean isCacheHit(String query);
 
+	/**
+	 * Performs a search using the given search query.
+	 *
+	 * @param query the search query
+	 * @return a list of results
+	 */
 	public List<Item> doSearch(String query);
 
+	/**
+	 * Removes an entry from the cache if it exists, does nothing otherwise.
+	 *
+	 * @param query the search query to remove
+	 */
 	public void remove(String query);
 
+	/**
+	 * Clears all entries from the cache.
+	 */
 	public void clear();
 }
